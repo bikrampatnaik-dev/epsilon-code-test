f3be35371bfcad07f529c8c20d68bdd99035e7e0
bikrampatnaik-dev <bikrampatnaik.dev@gmail.com>
2020-02-26 02:02:16 +0530

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
+  cards = [
+    {
+      imgUrl: "https://cdn.jsdelivr.net/gh/bikrampatnaik-dev/epsilon-code-test@master/src/assets/jake-oates-E38KRRk1fvg-unsplash.jpg",
+      headingStr : 'Beach Day'
+    }
+  ];
 }
