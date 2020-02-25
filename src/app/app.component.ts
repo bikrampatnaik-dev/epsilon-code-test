94403ffe3ae54548a366d620c1a2b94996a6f6da
bikrampatnaik-dev <bikrampatnaik.dev@gmail.com>
2020-02-26 03:14:31 +0530

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,15 @@ export class AppComponent {
   cards = [
     {
       imgUrl: "https://cdn.jsdelivr.net/gh/bikrampatnaik-dev/epsilon-code-test@master/src/assets/jake-oates-E38KRRk1fvg-unsplash.jpg",
-      headingStr : 'Beach Day'
+      headingStr : 'Beach Day',
+      title: 'Rufus the Dog',
+      modifiedBy: 'Modified 02/01/2020 by Jake Oates'
+    },
+    {
+      imgUrl: "https://stackblitz.com/files/epsilon-code-test/github/bikrampatnaik-dev/epsilon-code-test/master/src/assets/joe-caione.jpg",
+      headingStr: "Sunny Day",
+      title: 'Archie the Dog',
+      modifiedBy: 'Modified 03/01/2020 by Joe Caione'
     }
   ];
 }
